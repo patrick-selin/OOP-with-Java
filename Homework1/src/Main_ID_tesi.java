8fc2839555c78f3c563b4e60aa187e25552391e4
Patrick Selin <patrick.selin@gmail.com>
2023-02-16 08:08:01 +0200

    person id methods

--- Homework1/src/Main_ID_tesi.java
+++ Homework1/src/Main_ID_tesi.java
@@ -1,2 +1,35 @@
+import java.sql.SQLOutput;
+
 public class Main_ID_tesi {
+    public static void main(String[] args) {
+
+        Student student1 = new Student();
+
+        student1.setFirstName("Donald");
+        student1.setLastName("Duck");
+        student1.setBachelorCredits(120);
+        student1.setMasterCredits(180);
+        student1.setTitleOfMastersThesis("Masters thesis title");
+        student1.setTitleOfBachelorThesis("Bachelor thesis title");
+        student1.setStartYear(2001);
+        student1.setGraduationYear(2020);
+
+        student1.setPersonId("121298-830A");
+        student1.setPersonId("221199-123A");
+
+        System.out.println(student1.checkBirthdate("121298-830A"));
+
+
+        // System.out.println(student1);
+
+
+        // Print the output when setting the person id “This is a string”
+        // Print the output when setting the person id “320187-1234”
+        // Print the output when setting the person id ““11111111-3334”
+        // Print the output when setting the person id “121298-830A”
+
+
+
+    }
+
 }
--- Homework1/src/Student.java
+++ Homework1/src/Student.java
@@ -12,7 +12,7 @@ public class Student {
     private int graduationYear;
     private String birthDate = ConstantValues.NO_BIRTHDATE;
 
-    // ******* CONSTRACTORS *********
+    // ******* CONSTRUCTORS *********
     public Student() {
     }
 
@@ -23,7 +23,6 @@ public class Student {
 
     // ******* METHODS *********
 
-    //
     public String getFirstName() {
         return firstName;
     }
@@ -68,7 +67,6 @@ public class Student {
     }
 
     public double getBachelorCredits() {
-
         return bachelorCredits;
     }
 
@@ -135,7 +133,6 @@ public class Student {
     }
 
     public String setGraduationYear(final int graduationYear) {
-
         if (canGraduated() &&
             graduationYear >= this.startYear &&
             graduationYear <= getCurrentYear()) {
@@ -151,13 +148,10 @@ public class Student {
     }
 
     public boolean hasGraduated() {
-        boolean hasGraduated = this.graduationYear != 0;
-
-        return hasGraduated;
+        return this.graduationYear != 0;
     }
 
     private boolean canGraduated() {
-
         return bachelorCredits >= ConstantValues.BACHELOR_CREDITS &&
                 masterCredits >= ConstantValues.MASTER_CREDITS &&
                 !this.titleOfBachelorThesis.equals(ConstantValues.NO_TITLE) &&
@@ -165,24 +159,101 @@ public class Student {
     }
 
     public int getStudyYears () {
-
-        // **************
-        // tässä on virhe kun kun pitäis tulosaa 0
-        // ********************
-
         if (hasGraduated()) {
             return this.getGraduationYear() - this.getStartYear();
         }
         else {
             return (getCurrentYear() - this.getStartYear());
-
         }
     }
 
-    public String setPersonId(final int personId) {
+    // ***********************************
+    // ************** ID JUTUT************
+
+    
+
+    public String setPersonId(final String personId) {
         // check if id is correct (use checkPersoinIDNumber)
 
-        return "Ok";
+        if (checkPersonIDNumber(personId)) {
+            this.birthDate = personId;
+            return "Ok";
+        }
+
+        return "noooo";
+    }
+
+
+
+
+
+    // ***********************************
+    // ***********************************
+
+    private boolean checkPersonIDNumber(final String personId) {
+        return personId.length() == 11 &&
+                personId.charAt(6) == '-' ||
+                personId.charAt(6) == '+' ||
+                personId.charAt(6) == 'A';
+    }
+
+    private boolean checkLeapYear(int year) {
+        boolean isLeapYear;
+
+        isLeapYear = (year % 4 == 0);
+        isLeapYear = isLeapYear &&
+                     (year % 100 != 0 ||
+                      year % 400 == 0);
+
+        return isLeapYear;
+    }
+
+    // tämä ei sinällään vielä toimi, ehkä tehdä dictionary tästä,
+    // mutta katso ensin mitä testit sanoo kun palautat tehtävän
+    private boolean checkValidCharacter(final String personID) {
+        String validChars = "0123456789ABCDEFHJKLMNPRSTUVWXY";
+        String numbersStr = personID.substring(0,6) + personID.substring(7,10);
+
+        int number = Integer.parseInt(numbersStr);
+        int ControlChar= number % 31;
+
+        return (validChars.charAt(ControlChar) == personID.charAt(10));
+    }
+
+    public boolean checkBirthdate(final String date) {
+        boolean isValidDate = true;
+
+        String dayStr = date.substring(0,2);
+        int dayNum = Integer.parseInt(dayStr);
+        System.out.println(dayNum);
+
+        String monthStr = date.substring(2,4);
+        int monthNum = Integer.parseInt(monthStr);
+        System.out.println(monthNum);
+
+        String yearStr = date.substring(4,6);
+        int yearNum = Integer.parseInt(yearStr);
+        System.out.println(yearNum);
+
+        if (yearNum < 0 || monthNum < 1 || monthNum > 12 ||
+            dayNum < 1 || dayNum > 31) {
+            isValidDate = false;
+        }
+        else {
+            switch (monthNum) {
+                case 4, 6, 9, 11 -> {
+                    if (dayNum == 31)
+                        isValidDate = false;
+                }
+                case 2 -> {
+                    if (dayNum > 29)
+                        isValidDate = false;
+                    if (dayNum == 29)
+                        isValidDate = checkLeapYear(yearNum);
+                }
+            }
+        }
+        return isValidDate;
     }
 
     private String statusPrinter() {
@@ -204,11 +275,12 @@ public class Student {
                 tab + "FirstName: " + firstName + ", " +
                        "LastName: " + lastName + printL +
                 tab + "Status: " + statusPrinter() + printL +
-                tab + "StartYear: " + startYear + " (studies have lasted for " +
-                            getStudyYears() + " years)" + printL +
-                tab + "BachelorCredits: " + bachelorCredits + printL +
-                tab + "MasterCredits: " + masterCredits + printL +
-                tab + "TitleOfMastersThesis: " + titleOfMastersThesis + printL +
-                tab + "TitleOfBachelorThesis: " + titleOfBachelorThesis + printL;
+                tab + "Date of birth: " + this.birthDate + printL; // +
+                // tab + "StartYear: " + startYear + " (studies have lasted for " +
+                //            getStudyYears() + " years)" + printL +
+                // tab + "BachelorCredits: " + bachelorCredits + printL +
+                // tab + "MasterCredits: " + masterCredits + printL +
+                // tab + "TitleOfMastersThesis: " + titleOfMastersThesis + printL +
+                // tab + "TitleOfBachelorThesis: " + titleOfBachelorThesis + printL;
     }
 }
